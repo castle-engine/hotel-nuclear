05d662d72fee8c0b8dccea24ad2349320c96bd0d
Michalis Kamburelis <michalis.kambi@gmail.com>
2015-10-09 09:14:59 +0000

    HUGE ground-breaking (also, a little compatibility-breaking...) improvements to UI controls.
    * First of all, hierarchy of 2D items --- finally! TUIControl may have children, on Controls[] list. Events to them are automatically correctly passed. When rendering you need to look at ScreenRect to work in global coords.
    
    * TUIRectangularControl and TUIControl merged into one, which means that *every* TUIControl now has a Left/Bottom/Rect properties. This avoids useless complications.
    
    * Anchoring properties added on TUIControl. Have the same possibilities as old TCastleOnScreenMenu anchors, but now just work on any TUIControl. Old TCastleOnScreenMenu anchoring mechanism (PositionAbsolute,...) removed.
    
    * Sliders (float and integer) available in CastleControls unit. With OnChange callback (instead of previous AccessoryItemChanged on TCastleOnScreenMenu).
    
    * Large compatibility-breaking refactor of TCastleOnScreenMenu. It no longer manages the "accessory" list, or even string list, it completely depends now on TUIControl hierarchy to add items inside, and add accessiories to items. Where possible, simple TCastleLabel is used for text.
    
    * TCastleLabel.Frame is false by default, true was too annoying.
    
    * Order of controls (on Container.Controls, and on new TUIControl.Controls list) reversed: front is now last, this is more natural. To force upgrade, TUIControlList no longer descends from TObjectList, so it doesn't have Add and other methods. You will have to use InsertFront/InsertBack methods.
    
    * TCastleOnScreenMenu is unfortunately gone, was too complicated to restore. Maybe later will be resurrected, but for whole TUIControl.
    
    * PositionInside now magically always works, since everything is forced to define proper Rect value. You can also set CaptureAllEvents to capture keyboard, without the need to hack Rect value.
    
    * TUIControl now knows its parent (and there can be only one parent for TUIControl at a time! this disallows some dirty hacks.)
    
    * Add TUIControlFont.SmallFont, useful for sliders.
    
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/hotel_nuclear@14796 afe8dcc5-9094-451b-8773-5f495042a737

--- code/gameplay.pas
+++ code/gameplay.pas
@@ -111,8 +111,8 @@ var
   LineNum, I, X, Y: Integer;
 begin
   if Player.Dead then
-    GLFadeRectangle(ContainerRect, Red, 1.0) else
-    GLFadeRectangle(ContainerRect, Player.FadeOutColor, Player.FadeOutIntensity);
+    GLFadeRectangle(ParentRect, Red, 1.0) else
+    GLFadeRectangle(ParentRect, Player.FadeOutColor, Player.FadeOutIntensity);
 
   R := Rectangle(UIMargin, ContainerHeight - UIMargin - 120, 40, 120);
   DrawRectangle(R.Grow(2), Vector4Single(1.0, 0.5, 0.5, 0.2));
